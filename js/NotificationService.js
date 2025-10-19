252bf16df1e7891b217b53c79e448fbb51a98586
zsivm <zsivics.mihael@gmail.com>
2025-10-19 14:44:46 +0200

    create foundation for the app

---
+++ js/NotificationService.js
@@ -0,0 +1,16 @@
+export class NotificationService {
+    
+    static async requestPermission() {
+        if (Notification.permission !== 'granted') {
+            await Notification.requestPermission();
+        }
+    }
+
+    static send(title, body) {
+        if(Notification.permission === 'granted') {
+            new Notification(title, { body });
+        } else {
+            console.warn('Notification permission not granted.');
+        }
+    }
+}
---
+++ js/TimeUtils.js
@@ -0,0 +1,5 @@
+export function getTodayTime(timeString) {
+    const [hours, minutes] = timeString.split(':').map(Number);
+    const now = new Date();
+    return new Date(now.getFullYear(), now.getMonth(), now.getDate(), hours, minutes);
+}
